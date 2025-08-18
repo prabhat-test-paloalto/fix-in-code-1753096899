d7e9baf7e0212368476589271fd31f21a528d155
JainSiddhant <siddhant104@gmail.com>
2025-08-18 13:37:50 +0530

    Create terraform_0818.tf

---
+++ terraform_0818.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1" # change as needed
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # replace with a valid AMI ID in your region
+  instance_type = "t3.micro"
+
+  # Disable IMDSv2 requirement
+  metadata_options {
+    http_tokens = "optional"   # allows IMDSv1 (IMDSv2 not required)
+  }
+
+  tags = {
+    Name      = "siddhant-ec2-fix-in-code-check-2"
+    yor_trace = "d8ee6fc5-80d3-4f52-8e7e-9ed25684f8ab"
+  }
+}
